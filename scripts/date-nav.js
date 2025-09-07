120c541875da4693ee57e8844b71a9150b296537
theflash1932 <greg@gregnielson.com>
2025-09-07 10:24:39 -0600

    Fixed some validation items

--- scripts/date-nav.js
+++ scripts/date-nav.js
@@ -10,6 +10,6 @@ const navButton = document.querySelector('#nav-button');
 const navBar = document.querySelector('#nav-bar');
 
 navButton.addEventListener('click', () => {
-  navButton.classList.toggle('show');
-  navBar.classList.toggle('show');  
+    navButton.classList.toggle('show');
+    navBar.classList.toggle('show');
 });
